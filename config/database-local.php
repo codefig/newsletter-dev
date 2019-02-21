8a25682af78d481793b12274f57dd523b784beb5
Moshood Alaran <geekyshood@gmail.com>
2019-02-21 16:51:40 +0100

    added db files for heroku and local

--- config/database-local.php
+++ config/database-local.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'db4free.net',
-            'port' => 3306,
-            'database' =>'wedo_management',
-            'username' => 'wedo_admin',
-            'password' => 'aaaaaaaa',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
