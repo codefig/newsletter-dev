632d3065b8d44a07c5bec8a88f5c28bcc84b3b1d
Moshood Alaran <geekyshood@gmail.com>
2019-02-21 05:57:09 +0100

    made changes to db user structure

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,10 +63,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'token'  => str_random(30),
+            'has_paid' => null,
+            'is_subscribed' => null,
         ]);
     }
 }
