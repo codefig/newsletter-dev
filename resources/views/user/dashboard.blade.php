9e42ecdfed528021646be2f911b5afaf3da4d0ef
Moshood Alaran <geekyshood@gmail.com>
2019-11-08 04:23:02 +0100

    Email view now showing

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -627,14 +627,7 @@
             </div>
           </div>
         </main>
-        <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-          <span
-            >Copyright © 2017 Designed by
-            <a href="https://colorlib.com" target="_blank" title="Colorlib"
-              >Colorlib</a
-            >. All rights reserved.</span
-          >
-        </footer>
+
       </div>
     </div>
     <script type="text/javascript" src="{{ URL::to('static/js/vendor.js') }}"></script>
--- resources/views/user/email.blade.php
+++ resources/views/user/email.blade.php
@@ -56,7 +56,7 @@
             }
         }
     </style>
-  <link href="style.css" rel="stylesheet" />
+ <link href="{{ URL::to('static/css/style.css') }}" rel="stylesheet" />
 </head>
 
 <body class="app">
@@ -72,70 +72,73 @@
         });
     </script>
     <div>
-        <div class="sidebar">
-            <div class="sidebar-inner">
-                <div class="sidebar-logo">
-                    <div class="peers ai-c fxw-nw">
-                        <div class="peer peer-greed">
-                            <a class="sidebar-link td-n" href="index.html">
-                                <div class="peers ai-c fxw-nw">
-                                    <div class="peer">
-                                        <div class="logo"><img src="assets/static/images/logo.png" alt=""></div>
-                                    </div>
-                                    <div class="peer peer-greed">
-                                        <h5 class="lh-1 mB-0 logo-text">Adminator</h5></div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="peer">
-                            <div class="mobile-toggle sidebar-toggle"><a href="" class="td-n"><i class="ti-arrow-circle-left"></i></a></div>
-                        </div>
+      <div class="sidebar">
+        <div class="sidebar-inner">
+          <div class="sidebar-logo">
+            <div class="peers ai-c fxw-nw">
+              <div class="peer peer-greed">
+                <a class="sidebar-link td-n" href="index.html"
+                  ><div class="peers ai-c fxw-nw">
+                    <div class="peer">
+                      <div class="logo">
+                        <img src="assets/static/images/logo.png" alt="" />
+                      </div>
                     </div>
+                    <div class="peer peer-greed">
+                      <h5 class="lh-1 mB-0 logo-text">NewsLetter</h5>
+                    </div>
+                  </div></a
+                >
+              </div>
+              <div class="peer">
+                <div class="mobile-toggle sidebar-toggle">
+                  <a href="" class="td-n"
+                    ><i class="ti-arrow-circle-left"></i
+                  ></a>
                 </div>
-                <ul class="sidebar-menu scrollable pos-r">
-                    <li class="nav-item mT-30 active"><a class="sidebar-link" href="index.html"><span class="icon-holder"><i class="c-blue-500 ti-home"></i> </span><span class="title">Dashboard</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="email.html"><span class="icon-holder"><i class="c-brown-500 ti-email"></i> </span><span class="title">Email</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="compose.html"><span class="icon-holder"><i class="c-blue-500 ti-share"></i> </span><span class="title">Compose</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="calendar.html"><span class="icon-holder"><i class="c-deep-orange-500 ti-calendar"></i> </span><span class="title">Calendar</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="chat.html"><span class="icon-holder"><i class="c-deep-purple-500 ti-comment-alt"></i> </span><span class="title">Chat</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="charts.html"><span class="icon-holder"><i class="c-indigo-500 ti-bar-chart"></i> </span><span class="title">Charts</span></a></li>
-                    <li class="nav-item"><a class="sidebar-link" href="forms.html"><span class="icon-holder"><i class="c-light-blue-500 ti-pencil"></i> </span><span class="title">Forms</span></a></li>
-                    <li class="nav-item dropdown"><a class="sidebar-link" href="ui.html"><span class="icon-holder"><i class="c-pink-500 ti-palette"></i> </span><span class="title">UI Elements</span></a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-orange-500 ti-layout-list-thumb"></i> </span><span class="title">Tables</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a class="sidebar-link" href="basic-table.html">Basic Table</a></li>
-                            <li><a class="sidebar-link" href="datatable.html">Data Table</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-purple-500 ti-map"></i> </span><span class="title">Maps</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="google-maps.html">Google Map</a></li>
-                            <li><a href="vector-maps.html">Vector Map</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-red-500 ti-files"></i> </span><span class="title">Pages</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a class="sidebar-link" href="blank.html">Blank</a></li>
-                            <li><a class="sidebar-link" href="404.html">404</a></li>
-                            <li><a class="sidebar-link" href="500.html">500</a></li>
-                            <li><a class="sidebar-link" href="signin.html">Sign In</a></li>
-                            <li><a class="sidebar-link" href="signup.html">Sign Up</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-teal-500 ti-view-list-alt"></i> </span><span class="title">Multiple Levels</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                        <ul class="dropdown-menu">
-                            <li class="nav-item dropdown"><a href="javascript:void(0);"><span>Menu Item</span></a></li>
-                            <li class="nav-item dropdown"><a href="javascript:void(0);"><span>Menu Item</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="javascript:void(0);">Menu Item</a></li>
-                                    <li><a href="javascript:void(0);">Menu Item</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+              </div>
             </div>
+          </div>
+          <ul class="sidebar-menu scrollable pos-r">
+            <li class="nav-item mT-30 active">
+              <a class="sidebar-link" href="{{ route('home') }}"
+                ><span class="icon-holder"
+                  ><i class="c-blue-500 ti-home"></i> </span
+                ><span class="title">Dashboard</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="sidebar-link" href="{{ route('email') }}"
+                ><span class="icon-holder"
+                  ><i class="c-brown-500 ti-email"></i> </span
+                ><span class="title">Email</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="sidebar-link" href="{{ route('compose') }}"
+                ><span class="icon-holder"
+                  ><i class="c-blue-500 ti-share"></i> </span
+                ><span class="title">Compose</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="sidebar-link" href="calendar.html"
+                ><span class="icon-holder"
+                  ><i class="c-deep-orange-500 ti-calendar"></i> </span
+                ><span class="title">New Mail-list</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="sidebar-link" href="#"
+                ><span class="icon-holder"
+                  ><i class="c-deep-purple-500 ti-comment-alt"></i> </span
+                ><span class="title">Subscriber Lists</span></a
+              >
+            </li>
+
+          </ul>
         </div>
+      </div>
         <div class="page-container">
             <div class="header navbar">
                 <div class="header-container">
@@ -155,7 +158,7 @@
                                         <li>
                                             <a href="" class="peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100">
                                                 <div class="peer mR-15"><img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/1.jpg" alt=""></div>
-                                                <div class="peer peer-greed"><span><span class="fw-500">John Doe</span> <span class="c-grey-600">liked your <span class="text-dark">post</span></span>
+                                                <div class="peer peer-greed"><span><span class="fw-500">{{ $user->name }}</span> <span class="c-grey-600">liked your <span class="text-dark">post</span></span>
                                                     </span>
                                                     <p class="m-0"><small class="fsz-xs">5 mins ago</small></p>
                                                 </div>
@@ -581,8 +584,8 @@
             <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600"><span>Copyright © 2017 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span></footer>
         </div>
     </div>
-    <script type="text/javascript" src="assets/static/js/vendor.js"></script>
-    <script type="text/javascript" src="assets/static/js/bundle.js"></script>
+    <script type="text/javascript" src="{{ URL::to('static/js/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::to('static/js/bundle.js') }}"></script>
 </body>
 
 </html>
