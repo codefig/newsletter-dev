0768d48b5cadd2c085932dc012f61af73c32a7d6
Moshood Alaran <geekyshood@gmail.com>
2019-11-08 07:18:56 +0100

    Made changes to route file

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,13 @@
 */
 
 Route::get('/', 'SignUpController@index')->name('index');
-
 Route::get('/login', 'SignUpController@showLogin')->name('login');
-
 Route::get('/register', 'SignUpController@showRegister')->name('register');
-
 Route::post('/register', 'SignUpController@register')->name('register.post');
 Route::post('/login', 'SignUpController@login')->name('login.post');
-
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
 Route::get('/compose', 'HomeController@compose')->name('compose');
 Route::get('/email', 'HomeController@email')->name('email');
-
 Route::get('/logout', 'HomeController@logout')->name('logout');
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/verify/{token}', 'VerificationController@verifyEmail')->name('verify');
